252d3cb70f19c255a4c9241cb6c3e3904d47dc92
Ho-VietKhanh <kho20232@gmail.com>
2025-10-09 21:38:57 +0700

    Test some case

--- src/test/java/date/DateUtilsTest.java
+++ src/test/java/date/DateUtilsTest.java
@@ -28,9 +28,9 @@ public class DateUtilsTest {
     }
 
     @Test
-    void testCheckDate_InvalidCase() {
+    void testCheckDate_InvalidCase() {// Case này muốn passed thì phải ném ra một ngoại lệ IllegalArgumentException
         assertThrows(IllegalArgumentException.class, () -> DateUtils.checkDate(2021, 2, 30)); // Ngày không hợp lệ: 30 tháng 2
-        assertThrows(IllegalArgumentException.class, () -> DateUtils.checkDate(2021, 4, 31)); // Ngày không hợp lệ: 31 tháng 4
+        assertThrows(IllegalArgumentException.class, () -> DateUtils.checkDate(2021, 4, 30)); // Ngày không hợp lệ: 31 tháng 4
     }
 
     // Java
